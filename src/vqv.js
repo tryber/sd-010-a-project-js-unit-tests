0eaf45ca31045fb6ca41161f44f6ffb2134c696e
igson <igson660@gmail.com>
2021-03-26 15:39:38 -0500

    quesito 03

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
-
-module.exports = vqv;
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na trybe e mando muito em programação!
+  #VQV`
+};
+// module.exports = vqv;
